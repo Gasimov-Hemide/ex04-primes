c9c2ccc1cdf90aa7fbcb81d9bcc7b7cd9ead6dbb
Gasimov-Hemide <hemide.gasimov@edu.esiee.fr>
2024-11-07 11:04:15 +0000

    Voici mo programme pour l'exercice 4

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
+"""
+Verifie si un nombre est premier et affiche tous les nombres premiers
+modules : 
+    -isprime(p)
+    -main()
+"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """
+    Vérifie si un entier p est premier
+    args : 
+        -p : un entier dont on cherche s'il est premier
+    returns : 
+        -premier : un booléen, vraie si le nbr est premier sinon faux
+    """
+    if p <= 1:
+        return False
+    premier = True
+    for diviseur in range(2, int(sqrt(p)+1)):
+        if p % diviseur == 0:
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Fait des appels de test en utilisant le module isprime
+    """
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
